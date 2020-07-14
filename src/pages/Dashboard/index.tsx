9fc2ba16d7d27569d73fe8f8dd1720d5497178ff
Andre Melo <andre.fabian.melo@gmail.com>
2020-07-14 02:50:06 -0300

    Add List Orders Feature

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -55,11 +55,25 @@ const Dashboard: React.FC = () => {
 
   async function handleNavigate(id: number): Promise<void> {
     // Navigate do ProductDetails page
+    navigation.navigate('FoodDetails', { id });
   }
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
       // Load Foods from API
+      const foodResponse = await api.get<Food[]>('foods', {
+        params: {
+          name_like: searchValue,
+          category_like: selectedCategory,
+        },
+      });
+
+      setFoods(
+        foodResponse.data.map(food => ({
+          ...food,
+          formattedPrice: formatValue(food.price),
+        })),
+      );
     }
 
     loadFoods();
@@ -68,6 +82,9 @@ const Dashboard: React.FC = () => {
   useEffect(() => {
     async function loadCategories(): Promise<void> {
       // Load categories from API
+      const categoryResponse = await api.get<Category[]>('categories');
+
+      setCategories(categoryResponse.data);
     }
 
     loadCategories();
@@ -75,6 +92,12 @@ const Dashboard: React.FC = () => {
 
   function handleSelectCategory(id: number): void {
     // Select / deselect category
+    if (selectedCategory === id) {
+      setSelectedCategory(undefined);
+      return;
+    }
+
+    setSelectedCategory(id);
   }
 
   return (
--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -33,6 +33,7 @@ const Favorites: React.FC = () => {
   useEffect(() => {
     async function loadFavorites(): Promise<void> {
       // Load favorite foods from api
+      api.get('favorites').then(({ data }) => setFavorites(data));
     }
 
     loadFavorites();
